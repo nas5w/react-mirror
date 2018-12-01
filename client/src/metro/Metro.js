74cc4b3cd5e291d02c1b0e95b86ab645399d4718
Nick <7538045+nas5w@users.noreply.github.com>
2018-11-30 20:51:11 -0500

    Prevent metro image crash

--- client/src/metro/Metro.js
+++ client/src/metro/Metro.js
@@ -1,33 +1,44 @@
-import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrain } from '@fortawesome/free-solid-svg-icons';
-import './Metro.css';
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrain } from "@fortawesome/free-solid-svg-icons";
+import "./Metro.css";
 
-class Metro extends Component {
-  render() {
-    return (
-      <div className="Metro">
-        <div className="Train-icon">
-          <FontAwesomeIcon icon={faTrain} />
-        </div>
-        <div className="clear"></div>
-        <table className="Train-schedule">
-          <tbody>
-            {
-              this.props.metro &&
-              this.props.metro.map((train, key) => {
-                return <tr key={key} className="Train-row">
-                  <td><img alt={`${train.Line} line`} className="Line-image" src={require(`./${train.Line}.png`)} /></td>
+const Metro = props => {
+  return (
+    <div className="Metro">
+      <div className="Train-icon">
+        <FontAwesomeIcon icon={faTrain} />
+      </div>
+      <div className="clear" />
+      <table className="Train-schedule">
+        <tbody>
+          {props.metro &&
+            props.metro.map((train, key) => {
+              return (
+                <tr key={key} className="Train-row">
+                  <td>
+                    {(train.Line === "BL" ||
+                      train.Line === "OR" ||
+                      train.Line === "SV" ||
+                      train.Line === "RD" ||
+                      train.Line === "YL" ||
+                      train.Line === "GR") && (
+                      <img
+                        alt={`${train.Line} line`}
+                        className="Line-image"
+                        src={`./img/${train.Line}.png`}
+                      />
+                    )}
+                  </td>
                   <td>{train.Destination}</td>
                   <td>{train.Min}</td>
                 </tr>
-              })
-            }
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-}
+              );
+            })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default Metro;

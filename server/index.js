fe80aca214c593f09df2024ca0e14ddb03e45909
Nick <7538045+nas5w@users.noreply.github.com>
2018-11-30 21:34:45 -0500

    Refactor tests and change config file to environment variables

--- server/index.js
+++ server/index.js
@@ -1,20 +1,19 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors')
-const config = require('./config');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 const router = express.Router();
-const weather = require('./routes/weather');
-const metro = require('./routes/metro');
+const weather = require("./routes/weather");
+const metro = require("./routes/metro");
 
 app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post('/weather', weather);
-app.post('/metro', metro);
+app.post("/weather", weather);
+app.post("/metro", metro);
 
 app.listen(4000, () => {
-	console.log('Listening on port 4000');
+  console.log("Listening on port 4000");
 });
